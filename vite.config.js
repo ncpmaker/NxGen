36117e80ea60b78265fe6a37699a69ee328b6aef
Cramzzzberry <cramzzzberry@gmail.com>
2024-04-29 20:53:57 +0800

    Ready for github pages again

--- vite.config.js
+++ vite.config.js
@@ -2,57 +2,57 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-// import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
-    vue()
-    // VitePWA({
-    //   manifest: {
-    //     name: 'NxGen',
-    //     short_name: 'NxGen',
-    //     start_url: '/NxGen/',
-    //     display: 'standalone',
-    //     background_color: '#ecfdf5',
-    //     lang: 'en',
-    //     scope: '/NxGen/',
-    //     icons: [
-    //       {
-    //         src: 'icons/manifest-icon-192.maskable.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //         purpose: 'any'
-    //       },
-    //       {
-    //         src: 'icons/manifest-icon-192.maskable.png',
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //         purpose: 'maskable'
-    //       },
-    //       {
-    //         src: 'icons/manifest-icon-512.maskable.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any'
-    //       },
-    //       {
-    //         src: 'icons/manifest-icon-512.maskable.png',
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'maskable'
-    //       }
-    //     ]
-    //   }
-    // })
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'NxGen',
+        short_name: 'NxGen',
+        start_url: '/NxGen/',
+        display: 'standalone',
+        background_color: '#ecfdf5',
+        lang: 'en',
+        scope: '/NxGen/',
+        icons: [
+          {
+            src: 'icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable'
+          },
+          {
+            src: 'icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'icons/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'docs'
   }
-  // build: {
-  //   outDir: 'docs'
-  // }
 })
